a357aae9f3b2294253eb69b24f59034ff8d61bc0
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-06-12 17:42:37 -0300

    :sparkles: feat: Atualizado ex04

--- src/ex04/Main.java
+++ src/ex04/Main.java
@@ -3,11 +3,16 @@ package ex04;
 public class Main {
     public static void main(String[] args) {
         Carro carro1 = new Carro("Corolla", "Toyota", 2, 1.8);
-        Carro carro2 = new Carro("Golf", "Volkswagen", 4, 1.6);
-        Carro carro3 = new Carro("Civic", "Honda", 2, 1.5);
+        Carro carro2 = new Carro("Golf", "Volkswagen", 4);
 
         carro1.imprimirInformacoes();
         carro1.ligarCarro();
         carro1.imprimirInformacoes();
+
+        System.out.println();
+
+        carro2.imprimirInformacoes();
+        carro2.ligarCarro();
+        carro2.imprimirInformacoes();
     }
 }
